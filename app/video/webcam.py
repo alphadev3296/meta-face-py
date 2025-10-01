08ab65272d68652f578957b739b88c2a2b088722
alpha dev <alpha5611331@gmail.com>
2025-10-01 22:55:20 +0200

    added webcam props setting

--- app/video/webcam.py
+++ app/video/webcam.py
@@ -3,6 +3,13 @@ from pygrabber.dshow_graph import FilterGraph
 
 
 class Webcam:
+    def __init__(self, device: int, width: int, height: int, fps: int) -> None:
+        self.device = device
+        self.width = width
+        self.height = height
+        self.fps = fps
+        self.cap: cv2.VideoCapture | None = None
+
     @classmethod
     def list_webcams(cls) -> list[tuple[int, str]]:
         graph = FilterGraph()  # type: ignore  # noqa: PGH003
@@ -15,3 +22,22 @@ class Webcam:
                 available.append((i, name))
             cap.release()
         return available
+
+    def open(self) -> None:
+        if self.cap is not None:
+            self.cap.release()
+
+        self.cap = cv2.VideoCapture(self.device, cv2.CAP_DSHOW)
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, self.width)
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, self.height)
+        self.cap.set(cv2.CAP_PROP_FPS, self.fps)
+
+    def close(self) -> None:
+        self.cap.release()
+        self.cap = None
+
+    def read(self) -> tuple[bool, cv2.Mat]:
+        return self.cap.read()
+
+    def __del__(self) -> None:
+        self.close()
