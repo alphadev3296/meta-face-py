86b6643ce5a86185a878b6b50025d73a8b794d70
alpha dev <alpha5611331@gmail.com>
2025-10-17 11:06:53 +0200

    reverted fhd resolution

--- app/schema/camera_resolution.py
+++ app/schema/camera_resolution.py
@@ -4,9 +4,11 @@ from enum import StrEnum
 class CameraResolution(StrEnum):
     VGA = "VGA"
     HD = "HD"
+    FHD = "FHD"
 
 
 CAMERA_RESOLUTIONS: dict[CameraResolution, tuple[int, int]] = {
     CameraResolution.VGA: (640, 480),
     CameraResolution.HD: (1280, 720),
+    CameraResolution.FHD: (1920, 1080),
 }
