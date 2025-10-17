fbde7c03c6c15e2cac61622a93eb674df929efe3
alpha dev <alpha5611331@gmail.com>
2025-10-17 10:59:08 +0200

    removed some resolutions

--- app/schema/camera_resolution.py
+++ app/schema/camera_resolution.py
@@ -2,15 +2,11 @@ from enum import StrEnum
 
 
 class CameraResolution(StrEnum):
-    HD_4 = "HD/4"
     VGA = "VGA"
     HD = "HD"
-    FHD = "FHD"
 
 
 CAMERA_RESOLUTIONS: dict[CameraResolution, tuple[int, int]] = {
-    CameraResolution.HD_4: (640, 360),
     CameraResolution.VGA: (640, 480),
     CameraResolution.HD: (1280, 720),
-    CameraResolution.FHD: (1920, 1080),
 }
