2d5e140f02f8c02d725025030bc69837df858950
alpha dev <alpha5611331@gmail.com>
2025-10-17 10:34:02 +0200

    adjusted processed view

--- app/ui/video_preview.py
+++ app/ui/video_preview.py
@@ -24,7 +24,6 @@ class VideoPanel(ttk.Frame):
         # Camera view
         self.camera_frame = ttk.LabelFrame(self, text="Camera View", padding=5)
         self.camera_frame.grid(row=0, column=0, sticky="nsew", padx=5, pady=5)
-
         self.camera_frame.rowconfigure(0, weight=0)
         self.camera_frame.rowconfigure(1, weight=1)
         self.camera_frame.columnconfigure(0, weight=1)
@@ -48,6 +47,9 @@ class VideoPanel(ttk.Frame):
         # Processed view
         self.processed_frame = ttk.LabelFrame(self, text="Processed View", padding=5)
         self.processed_frame.grid(row=0, column=1, sticky="nsew", padx=5, pady=5)
+        self.processed_frame.rowconfigure(0, weight=0)
+        self.processed_frame.rowconfigure(1, weight=1)
+        self.processed_frame.columnconfigure(0, weight=1)
 
         self.show_processed_var = tk.BooleanVar(value=True)
         self.show_processed_cb = ttk.Checkbutton(
